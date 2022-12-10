b1a95787d5573562d7c52af06ff37757c098f64b
nishadahmed5 <nishatahmed650@gmail.com>
2022-12-10 23:10:49 +0600

    final commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion , ObjectId} = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -20,13 +20,46 @@ app.get('/', (req, res) =>{
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tbf6iah.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
  async function run() {
     try {
-        const todoCollection = client.db('TODO').collection(todoItems)
-        
+        const todoCollection = client.db('TODO').collection('todoItems')
+
+
+
+        app.get('/items', async(req, res)=>{
+            const query = {}
+            const result = await todoCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.post('/items', async(req, res)=>{
+            const query = req.body;
+            const result = await todoCollection.insertOne(query)
+            res.send(result)
+        })
+
+        app.put('/items/:id', async(req, res)=>{
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id)}
+            const item = req.body
+            const option = { upsert:true }
+            updatedDoc = {
+                $set: {
+                    name: item.name
+                }
+            }
+            const result = await todoCollection.updateOne(filter, updatedDoc, option)
+            res.send(result)
+        })
+
+        app.delete('/items/:id', async(req, res)=>{
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await todoCollection.deleteOne(filter)
+            res.send(result)
+        } )
 
     }
     finally{
